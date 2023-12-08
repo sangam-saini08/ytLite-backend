765b7d52c304b51fbf66ab1357f83ad3aef636be
sangam saini <sangamsaini779@gmail.com>
2023-12-08 13:05:37 +0530

    add the handler files for error and the response

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is listening on : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection Failed !!!", err);
+    process.exit(1);
+  });

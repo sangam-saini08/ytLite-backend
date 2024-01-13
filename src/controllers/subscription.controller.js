cb5acf0aa7606a954530a834fb39f676caace2f3
sangam saini <sangamsaini779@gmail.com>
2024-01-13 22:35:32 +0530

    add getUserChannelSubscribers and getSubscribedChannels end point in subscription.controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,7 +9,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
   const subscriberId = req.user._id;
 
-  console.log("this is the channle Id :", channelId);
+  // console.log("this is the channle Id :", channelId);
 
   if (!isValidObjectId(channelId)) {
     throw new ApiError(400, "Invalid channel Id"); // check if channelId is valid
@@ -37,4 +37,141 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   res.status(201).json(new ApiRespones(201, {}, "subscribed"));
 });
 
-export { toggleSubscription };
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel Id"); // check if channelId is valid
+  }
+
+  const channel = await User.findById(channelId);
+
+  if (!channel) {
+    throw new ApiError(400, "User not found in this channel Id");
+  }
+
+  const subscribers = await User.aggregate([
+    {
+      // first pipline
+      $match: {
+        username: channel.username,
+      },
+    },
+    {
+      // second pipline
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      // third pipline
+      $lookup: {
+        from: "users",
+        localField: "subscribers.subscriber",
+        foreignField: "_id",
+        as: "subscribersList",
+        pipeline: [
+          {
+            // first sub pipline
+            $project: {
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    // {
+    //   // forth pipline
+    //   $project: {
+    //     subscribersList: 1,
+    //   },
+    // },
+  ]);
+
+  // console.log(subscribers[0].subscribersList);
+  res
+    .status(200)
+    .json(
+      new ApiRespones(
+        200,
+        subscribers[0].subscribersList,
+        "list fetched successfully"
+      )
+    );
+});
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel Id"); // check if channelId is valid
+  }
+
+  const channel = await User.findById(channelId);
+
+  if (!channel) {
+    throw new ApiError(400, "User not found in this channel Id");
+  }
+
+  const subscribedTo = await User.aggregate([
+    {
+      // first pipline
+      $match: {
+        username: channel.username,
+      },
+    },
+    {
+      // second pipline
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      // third pipline
+      $lookup: {
+        from: "users",
+        localField: "subscribedTo.channel",
+        foreignField: "_id",
+        as: "subscribedToList",
+        pipeline: [
+          {
+            // first sub pipline
+            $project: {
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    // {
+    //   // forth pipline
+    //   $project: {
+    //     subscribedToList: 1,
+    //   },
+    // },
+  ]);
+
+  res
+    .status(200)
+    .json(
+      new ApiRespones(
+        200,
+        subscribedTo[0].subscribedToList,
+        "list fetched successfully"
+      )
+    );
+});
+
+export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import verifyJwt from "../middlewares/auth.middleware.js";
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import {
+  toggleSubscription,
+  getSubscribedChannels,
+  getUserChannelSubscribers,
+} from "../controllers/subscription.controller.js";
 
 const router = Router();
 router.use(verifyJwt);
 
-router.route("/c/:channelId").post(toggleSubscription);
+router
+  .route("/c/:channelId")
+  .get(getSubscribedChannels)
+  .post(toggleSubscription);
+
+router.route("/u/:channelId").get(getUserChannelSubscribers);
 
 export default router;

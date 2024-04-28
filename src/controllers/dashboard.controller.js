ba3692dfe13c1be1e1e62ba59ba380410c5908f9
sangam saini <sangamsaini779@gmail.com>
2024-04-28 22:44:52 +0530

    get the channel video in the deshboard controller done

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -19,16 +19,62 @@ const getChannelStats = asyncHandler(async (req, res) => {
     { $group: { _id: null, totalViews: { $sum: "$views" } } },
   ]);
 
+  const totalLikes = await Video.aggregate([
+    { $match: { owner: user._id } },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "Video",
+        as: "likes",
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalLikes: { $sum: { $size: "$likes" } },
+      },
+    },
+  ]);
+
+  console.log("Total likes for all videos:", totalLikes);
+
   const stats = {
     videos: totalVideos,
     currentSubscribers: totalSubscribers,
     views: totalVideoViews[0].totalViews,
+    Likes: totalLikes[0].totalLikes,
   };
-  return res.json(new ApiRespones(200, stats, "worked"));
+  return res.json(new ApiRespones(200, stats, "stats fetched successfully "));
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
   // TODO: Get all the videos uploaded by the channel
+  const user = req.user;
+  let sortBy = req.query.sortBy || "latest";
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize) || 10;
+
+  let sortCriteria = {};
+
+  if (sortBy === "latest") {
+    sortCriteria = { createdAt: -1 };
+  } else if (sortBy === "popular") {
+    sortCriteria = { views: -1 };
+  } else if (sortBy === "oldest") {
+    sortCriteria = { createdAt: 1 };
+  } else {
+    throw new ApiError(400, "Invalid sortBy parameter");
+  }
+
+  const videos = await Video.aggregate([
+    { $match: { owner: user._id } },
+    { $sort: sortCriteria },
+    { $skip: (page - 1) * pageSize },
+    { $limit: pageSize },
+  ]);
+
+  return res.json(new ApiRespones(200, videos, "vides fetched successfull"));
 });
 
 export { getChannelStats, getChannelVideos };
